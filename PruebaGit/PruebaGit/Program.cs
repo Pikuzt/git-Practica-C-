728c53804f49aaa7f4362df6807cbc4876e6e9a6
jose carlos <castillo.villa@nube.unadmexico.mx>
2020-09-21 16:24:50 -0500

    proyecto c#

---
+++ PruebaGit/PruebaGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
